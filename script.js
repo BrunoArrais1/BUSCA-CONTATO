5c79c457a717f116a5a8e75b996f9a61db883d89
BrunoArrais1 <brunosarrais@gmail.com>
2026-05-08 10:35:11 -0300

    script.js

--- script.js
+++ script.js
@@ -1,39 +1,71 @@
 let baseMunicipio = [];
 let baseINEP = [];
 
-// 🔥 ler Excel com múltiplas abas
+// =========================
+// CARREGAR EXCEL
+// =========================
 fetch("dados.xlsx")
   .then(res => res.arrayBuffer())
   .then(data => {
 
     let workbook = XLSX.read(data);
 
-    // 👉 ABA 1 (municipio)
+    // ABA 1 = MUNICIPIO
     let aba1 = workbook.Sheets[workbook.SheetNames[0]];
     baseMunicipio = XLSX.utils.sheet_to_json(aba1);
 
-    // 👉 ABA 2 (inep)
+    // ABA 2 = INEP
     let aba2 = workbook.Sheets[workbook.SheetNames[1]];
     baseINEP = XLSX.utils.sheet_to_json(aba2);
+
+    // 👉 gerar lista automática
+    carregarMunicipios();
   });
 
-// =====================
-// TROCAR ABAS VISUAIS
-// =====================
+// =========================
+// GERAR LISTA AUTOMÁTICA
+// =========================
+function carregarMunicipios() {
+
+    let select = document.getElementById("municipio");
+
+    // limpar
+    select.innerHTML = '<option value="">Selecione o município</option>';
+
+    // pegar lista única
+    let municipios = [...new Set(baseMunicipio.map(x => x["MUNICIPIO"]))];
+
+    // ordenar
+    municipios.sort();
+
+    // preencher
+    municipios.forEach(m => {
+        if (m) {
+            let opt = document.createElement("option");
+            opt.value = m;
+            opt.textContent = m;
+            select.appendChild(opt);
+        }
+    });
+}
+
+// =========================
+// TROCAR ABAS
+// =========================
 function mostrarAba(tipo) {
     document.getElementById("abaMunicipio").style.display = tipo === 'mun' ? 'block' : 'none';
     document.getElementById("abaINEP").style.display = tipo === 'inep' ? 'block' : 'none';
 }
 
-// =====================
+// =========================
 // BUSCA MUNICIPIO
-// =====================
+// =========================
 function buscarMunicipio() {
 
-    let m = document.getElementById("municipio").value.toLowerCase();
+    let m = document.getElementById("municipio").value;
 
     let r = baseMunicipio.find(x =>
-        x["MUNICIPIO"] && x["MUNICIPIO"].toLowerCase() === m
+        x["MUNICIPIO"] === m
     );
 
     let div = document.getElementById("resMun");
@@ -44,21 +76,19 @@ function buscarMunicipio() {
     }
 
     div.innerHTML = `
-        <b>Nome Secretário:</b> ${r["NOME - SECRETÁRIO"] || "NÃO LOCALIZADO"}<br>
-        <b>Email:</b> ${r["E-MAIL - SECRETÁRIO"] || "NÃO LOCALIZADO"}
+        <b>Nome:</b> ${r["NOME RESPONSÁVEL"]}<br>
+        <b>Email:</b> ${r["EMAIL COMERCIAL"]}
     `;
 }
 
-// =====================
+// =========================
 // BUSCA INEP
-// =====================
+// =========================
 function buscarINEP() {
 
     let i = document.getElementById("inep").value;
 
-    let r = baseINEP.find(x =>
-        x["CODIGO INEP"] == i
-    );
+    let r = baseINEP.find(x => x["CODIGO INEP"] == i);
 
     let div = document.getElementById("resINEP");
 
@@ -71,7 +101,9 @@ function buscarINEP() {
         <b>Nome da Escola:</b> ${r["NOME ESCOLA"]}<br>
         <b>Município:</b> ${r["MUNICIPIO"]}<br>
         <b>Responsável:</b> ${r["NOME RESPONSÁVEL"]}<br>
-        <b>Email Responsável:</b> ${r["EMAIL COMERCIAL"]}<br>
-        <b>Telefone Responsável:</b> ${r["TELEFONE COMERCIAL"]}<br>
+        <b>Email:</b> ${r["EMAIL COMERCIAL"]}<br>
+        <b>Telefone:</b> ${r["TELEFONE COMERCIAL"]}<br>
+        <b>Email Instituição:</b> ${r["EMAIL INSTITUICAO"] || "NÃO LOCALIZADO"}<br>
+        <b>Telefone Instituição:</b> ${r["TELEFONE INSTITUICAO"] || "NÃO LOCALIZADO"}
     `;
 }
